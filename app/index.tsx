4b4e549bbf317a53948c288a81d56bd69d96520b
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2025-07-12 15:58:35 +0100

    chore: implement index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,71 @@
-import { useAssets } from 'expo-asset';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { Link } from "expo-router";
+import { defaultStyles } from '@/constants/Styles';
+import Colors from "@/constants/Colors";
 
 const Page = () => {
-  const [assets] = useAssets([require('@/assets/fonts/SpaceMono-Regular.ttf')]);
-  return (
-    <View>
-      <Text>Pagess</Text>
-    </View>
-  )
-}
+  const assetId = require("@/assets/videos/intro.mp4");
 
-export default Page
+  const player = useVideoPlayer(assetId, (player) => {
+    player.loop = true;
+    player.muted = true;
+    player.play();
+  });
+
+  return (
+    <View style={styles.container}>
+      {assetId && <VideoView style={styles.video} player={player} />}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Link
+          href={'/login'}
+          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: Colors.dark }]}
+          asChild>
+          <TouchableOpacity>
+            <Text style={{ color: 'white', fontSize: 22, fontWeight: '500' }}>Log in</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link
+          href={'/signup'}
+          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: '#fff' }]}
+          asChild>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: '500' }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
+  },
+});
+
+export default Page;
